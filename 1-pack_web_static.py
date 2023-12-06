03530b0f0c4c5234bd7ee72102b570456ee37645
SherrynneJoy <sherrynnembuvi@gmail.com>
2023-12-06 07:21:45 -0800

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,7 @@ def do_pack():
     date = datetime.now()
     timestamp = date.strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(timestamp)
-    res = local("tar -czvf {} web_static".format(archive_name), capture=True)
+    res = local("tar -czvf {} web_static".format(archive_name))
     if res.succeeded:
         return archive_name
     else:
